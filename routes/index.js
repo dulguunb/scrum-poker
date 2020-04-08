a730332e18fdfc1bc4be3c8cd59fcb3d4f482e43
dulguun <dulgunn.bold@ericsson.com>
2020-04-08 20:56:18 +0200

    Adding sockets first configuration  - client connection  - server connection

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
+var http = require('http');
+var server = http.createServer(express);
+var server  = require('http').createServer(express);
+var io      = require('socket.io').listen(server);
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express',
+    socketVersion:'https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.js'}
+  );
+});
+router.get('/:id', function(req, res, next) {
+  const roomName = req.params.id;
+  res.render('index', {
+    title: 'Express',
+    socketVersion:'https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.js'}
+  );
 });
 
+io.on('connection', function(socket) {
+  console.log('a user connected');
+});
 module.exports = router;
