285a10b62b1ce1ea71603c9ffde01ef7f83aeb6c
dulguun <dulgunn.bold@ericsson.com>
2020-04-10 21:32:11 +0200

    added a new /poker namespace and database
     - server: added a new database logic for the rooms and namespace for the socket
     - client: added a new event listeners and passing params from jade template to js
     - server: Added a new dbhandler module

---
+++ socket/dbHandler.js
@@ -0,0 +1,33 @@
+var Datastore = require('nedb')
+  , db = new Datastore();
+
+module.exports = {
+  database:db,
+  insert: (data) => {
+      db.insert(data,function(err,newDocs){
+        if(err){
+          console.log('an error occured');
+          console.log(err);
+        }
+        else{
+          console.log('a new document is inserted:');
+          console.log(newDocs);
+        }
+      });
+  },
+  remove : (data) => {
+    db.remove(data,function(err,numRemoved){
+      if(err){
+        console.log('an error occured');
+        console.log(err);
+      }
+      else{
+        console.log('a new document is inserted');
+        console.log(numRemoved);
+      }
+    });
+  },
+  find : (query,callback) => {
+    this.database.find(query,callback);
+  }
+}
